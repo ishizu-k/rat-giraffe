6cd6b281a02c25e45c1b376d003ad75fbee9e61f
ishizu <demekinn_96@yahoo.co.jp>
2019-06-19 10:36:57 +0900

    add: rentsコントローラのアクション設定

--- app/controllers/rents_controller.rb
+++ app/controllers/rents_controller.rb
@@ -1,2 +1,23 @@
 class RentsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
 end
