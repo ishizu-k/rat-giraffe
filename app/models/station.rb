d6a7aba04449461549bd88ac544e18b8baafb9f9
ishizu <demekinn_96@yahoo.co.jp>
2019-06-20 16:30:13 +0900

    add: Stationモデルにバリデーションを設定（路線名・駅名に文字数制限、徒歩分数は数値であること）

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,3 +1,6 @@
 class Station < ApplicationRecord
   belongs_to :rent, optional: true
+  validates :line_name, length: { maximum: 20 }
+  validates :station_name, length: { maximum: 20 }
+  validates :walk, numericality: true, allow_blank: true
 end
