c3898121d65df557790d43070468f03859033734
ishizu <demekinn_96@yahoo.co.jp>
2019-06-20 16:13:07 +0900

    add: Rentモデルにバリデーションを設定（物件名・賃料・住所・築年数は空でないこと。物件名・住所・備考に文字数制限。賃料・築年数は数値であること。賃料は1円以上であること）

--- app/models/rent.rb
+++ app/models/rent.rb
@@ -1,4 +1,9 @@
 class Rent < ApplicationRecord
   has_many :stations
   accepts_nested_attributes_for :stations
+  validates :property_name, presence: true, length: { in: 1..50 }
+  validates :fee, presence: true, numericality: { greater_than_or_equal_to: 1 }
+  validates :address, presence: true
+  validates :age, presence: true, numericality: true
+  validates :note, length: { maximum: 200 }
 end
